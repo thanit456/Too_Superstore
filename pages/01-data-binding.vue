d678b663826f81d4c980883054601e537d9166ef
thanit456 <too_w_o@hotmail.com>
2019-11-13 22:11:35 +0700

    first try

---
+++ pages/01-data-binding.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="con-center">
+        <nuxt-link to="/about">About</nuxt-link>
+        <h1 v-if="ok">Hello {{name}}</h1>
+        <div v-show="ok">UP NAME - {{upperName}}</div>
+        <input type="text" :value="name"/>
+        <input type="text" v-model="name"/>
+        <button @click="doSave">Save</button>
+        <v-btn color="success" @click="doToggle">Toggle</v-btn>
+   
+        <h1>Student List</h1>
+        <ul>
+            <li v-for="st in student" :key="st.code"
+            :class="{fail:st.score<50,good:st.score>=80}">
+                <span>{{st.code}}</span>
+                <span>{{st.name}}</span>
+            </li>
+        </ul> 
+
+        <ul>
+            <li v-for="(uname, index) in namelists" :key="index">
+                {{uname}}
+            </li>
+        </ul> 
+        
+    
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        setInterval(() => {
+            this.name += "+";
+        }, 5000);
+
+        return {
+             name: "",
+             namelists: [],
+             ok: true,
+             student: [
+                 {code:'001', name:'Too', score:50},
+                 {code:'002', name:'Win', score:80},
+                 {code:'003', name:'Boss', score:20}
+             ]
+        }
+    },
+    watch: {
+        namelists() {
+            if (this.namelists.length > 4) {
+                alert("Length: " + this.namelists.length);
+            }
+        }
+    },
+    computed: {
+            upperName() { 
+                return this.name.toUpperCase()
+            }
+    },
+    methods: {
+        doSave() {
+            console.log('SAVE');
+            this.namelists.push(this.name);
+            this.name = "";
+        },
+        doToggle() {
+            this.ok = !this.ok;
+        },
+    },
+}
+</script>
+
+<style scoped>
+    .con-center {
+        text-align: center;
+        margin-top: 1rem;
+    }
+    .fail {
+        color: red;
+    }
+    .good {
+        color: green;
+        font-weight: bold;
+    }
+</style>
---
+++ plugins/vuetify.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+// import 'vuetify/dist/vuetify.min.css' // Ensure for using css-loader
+// import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+    theme: {
+        dark: false,
+    },
+})
