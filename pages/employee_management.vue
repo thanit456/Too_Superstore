5ad77dc458d28922c382e7fd067ddfd1b04bb978
thanit456 <too_w_o@hotmail.com>
2019-11-14 23:07:17 +0700

    add employee table

--- pages/employee_management.vue
+++ pages/employee_management.vue
@@ -1,38 +1,67 @@
 <template>
     <div class="input_info">
-           
-        <v-row>
-            <v-col cols="50" sm="6" md="5">
-                <v-text-field class="text_field" type="text" v-model="name"
-                label="Username" placeholder="Fill in the blank"></v-text-field> 
-                <v-text-field class="text_field" type="text" v-model="email"
-                label="Email" placeholder="Fill in the blank"></v-text-field> 
-            </v-col>
+    
+        <v-row>    
+                <v-col cols="12" sm="6" md="2" offset-md="5"
+                class="column" 
+                :align-self="center">
+                <v-text-field
+                    v-model="name"
+                    label="Username"
+                    placeholder="Fill in the blank"
+                ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="2" offset-md="5"
+                class="column">
+                <v-text-field
+                    v-model="email"
+                    label="Email"
+                    placeholder="Fill in the blank"
+                ></v-text-field>
+                </v-col>
         </v-row>
         
         <v-btn @click="doSave">
             <v-icon dark>favorite</v-icon>Save
         </v-btn>
 
+        <v-simple-table>
+           
+                <thead>
+                    <tr>
+                        <th class="text-left">EmployeeID</th>
+                        <th class="text-left">Name</th>
+                        <th class="text-left">Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="employee in employeeLists" :key='employee.number'>
+                        <td>{{employee.number}}</td>
+                        <td>{{employee.name}}</td>
+                        <td>{{employee.email}}</td>
+                    </tr>
+                </tbody>
+        
+        </v-simple-table>
+    
         <!-- <div>
             <ul>
                 <li v-for="(employee, index) in employeeLists" :key='index'>
                     <span>{{employee.number}}</span>    
-                    <hr>
+                    
                     <span>{{employee.name}}</span>    
-                    <hr>
+                    
                     <span>{{employee.email}}</span>    
-                    <hr>
-
+                    
 
                 </li>
             </ul>
         </div> -->
 
-        <v-data-table
+        <!-- <v-data-table
         ::headers="headers"
         ::items="employeeLists"
-        ></v-data-table>
+        ></v-data-table> -->
         
 
     </div>
@@ -49,27 +78,6 @@ export default {
                  {number: 1, name: 'Too', email: 'too_w_o@hotmail.com'},
              ],
              ok: true,
-             headers: [
-                 {
-                    text: 'Employee number',
-                    align: 'left',
-                    sortable: false,
-                    width: '100px',
-                    value: 'number'
-                 },
-                 {
-                     text: 'Name',
-                     value: 'name',
-                     width: '100px',
-                 },
-                 {
-                    text: 'Email',
-                    value: 'email',
-                    width: '100px',
-                 }
-             ],
-             width: '200px',
-
              
         }
     },
@@ -91,6 +99,7 @@ export default {
             this.latestNumber += 1;
             this.employeeLists.push({number: this.latestNumber, name: this.name, email: this.email});
             this.name = "";
+            this.email = "";
         },
         doToggle() {
             this.ok = !this.ok;
@@ -106,5 +115,9 @@ export default {
 }
 .text_field {
     background-color: sandybrown;
+
+}
+.column {
+    
 }
 </style>
