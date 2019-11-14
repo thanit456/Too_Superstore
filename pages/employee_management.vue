fdef2a3e7f7259862f8f6363a02b5cbd54896741
thanit456 <too_w_o@hotmail.com>
2019-11-14 12:59:56 +0700

    fix bug in a table

--- pages/employee_management.vue
+++ pages/employee_management.vue
@@ -1,31 +1,39 @@
 <template>
     <div class="input_info">
-        
            
-                <v-row>
-                    <v-col cols="50" sm="6" md="5">
-                        <v-text-field type="text" v-model="name"
-                        label="Username" placeholder="Fill in the blank"></v-text-field> 
-                        <v-text-field type="text" v-model="email"
-                        label="Email" placeholder="Fill in the blank"></v-text-field> 
-                    </v-col>
-                </v-row>
-            
+        <v-row>
+            <v-col cols="50" sm="6" md="5">
+                <v-text-field class="text_field" type="text" v-model="name"
+                label="Username" placeholder="Fill in the blank"></v-text-field> 
+                <v-text-field class="text_field" type="text" v-model="email"
+                label="Email" placeholder="Fill in the blank"></v-text-field> 
+            </v-col>
+        </v-row>
         
-        <v-btn class='text_field' @click="doSave">
+        <v-btn @click="doSave">
             <v-icon dark>favorite</v-icon>Save
         </v-btn>
 
-
-        <div>
+        <!-- <div>
             <ul>
                 <li v-for="(employee, index) in employeeLists" :key='index'>
+                    <span>{{employee.number}}</span>    
+                    <hr>
                     <span>{{employee.name}}</span>    
+                    <hr>
                     <span>{{employee.email}}</span>    
+                    <hr>
+
 
                 </li>
             </ul>
-        </div>
+        </div> -->
+
+        <v-data-table
+        ::headers="headers"
+        ::items="employeeLists"
+        ></v-data-table>
+        
 
     </div>
 </template>
@@ -36,10 +44,32 @@ export default {
         return {
              name: "",
              email: "",
+             latestNumber: 1,
              employeeLists: [
-                 {name: 'Too', email: 'too_w_o@hotmail.com'},
+                 {number: 1, name: 'Too', email: 'too_w_o@hotmail.com'},
              ],
              ok: true,
+             headers: [
+                 {
+                    text: 'Employee number',
+                    align: 'left',
+                    sortable: false,
+                    width: '100px',
+                    value: 'number'
+                 },
+                 {
+                     text: 'Name',
+                     value: 'name',
+                     width: '100px',
+                 },
+                 {
+                    text: 'Email',
+                    value: 'email',
+                    width: '100px',
+                 }
+             ],
+             width: '200px',
+
              
         }
     },
@@ -58,7 +88,8 @@ export default {
     methods: {
         doSave() {
             console.log('SAVE');
-            this.employeeLists.push({name: this.name, email: this.email});
+            this.latestNumber += 1;
+            this.employeeLists.push({number: this.latestNumber, name: this.name, email: this.email});
             this.name = "";
         },
         doToggle() {
